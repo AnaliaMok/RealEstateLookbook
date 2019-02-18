ff2a09c6d7747f17c1021dae0918a11b661724a7
Analia Mok <analiam319@gmail.com>
2019-02-18 12:33:47 -0500

    Adjusted styling for page controls

--- src/views/Lookbook.vue
+++ src/views/Lookbook.vue
@@ -11,35 +11,37 @@
       :lookbookID="lookbook.sys.id"
     />
     <div class="lookbook__controls">
-      <router-link to="/">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          class="home-icon"
-          alt="Back To Home"
-        >
-          <path d="M8 20H3V10H0L10 0l10 10h-3v10h-5v-6H8v6z"></path>
-        </svg>
-      </router-link>
-      <button class="lookbook__controls__control" @click="goToPrevPage">
-        <!-- Left Chevron -->
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-          <path
-            d="M7.05 9.293L6.343 10 12 15.657l1.414-1.414L9.172 10l4.242-4.243L12 4.343z"
-          ></path>
-        </svg>
-        <span>Previous</span>
-      </button>
-      <div class="lookbook__controls__curr">{{ currentPage + 2 }}</div>
-      <button class="lookbook__controls__control" @click="goToNextPage">
-        <span>Next</span>
-        <!-- Right Chevron -->
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-          <path
-            d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"
-          ></path>
-        </svg>
-      </button>
+      <div class="lookbook__controls__container">
+        <router-link to="/">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            class="home-icon"
+            alt="Back To Home"
+          >
+            <path d="M8 20H3V10H0L10 0l10 10h-3v10h-5v-6H8v6z"></path>
+          </svg>
+        </router-link>
+        <button class="lookbook__controls__control" @click="goToPrevPage">
+          <!-- Left Chevron -->
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path
+              d="M7.05 9.293L6.343 10 12 15.657l1.414-1.414L9.172 10l4.242-4.243L12 4.343z"
+            ></path>
+          </svg>
+          <span>Previous</span>
+        </button>
+        <div class="lookbook__controls__curr">{{ currentPage + 2 }}</div>
+        <button class="lookbook__controls__control" @click="goToNextPage">
+          <span>Next</span>
+          <!-- Right Chevron -->
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path
+              d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"
+            ></path>
+          </svg>
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -124,16 +126,28 @@ export default {
     .home-icon {
       margin: 1rem;
       transition: all 0.2s;
-      fill: $orange;
+      fill: $slate;
 
       &:hover {
         fill: $orange;
       }
     }
 
+    &__container {
+      display: flex;
+      align-items: center;
+      background-color: $off-white;
+      padding-left: 1rem;
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.25);
+      border-bottom-left-radius: 1rem;
+    }
+
     &__curr {
       font-weight: $semibold;
       font-size: 1.8rem;
+      padding: 1rem;
+      background-color: $off-white;
+      color: $slate;
     }
 
     button {
@@ -142,9 +156,8 @@ export default {
       justify-content: center;
       outline: none;
       border: none;
-      border-radius: 5px;
       background-color: $off-white;
-      box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.25);
+      color: $slate;
       margin: 1rem;
       padding: 0 1rem;
       font-family: $secondary-font;
@@ -153,6 +166,7 @@ export default {
 
       svg {
         margin: 0.5rem;
+        fill: $slate;
       }
     }
   }
